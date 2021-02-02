b9d2d87b937a88afd89215e5f6eb039d44324bc2
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 12:49:33 +0900

    정보 올림피아드 1809 스택

---
+++ 정등혁/정보올림피아드/자료구조/Jungol1809/main.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n;
+stack<pair<int, int>> _stack;
+int answer[500001];
+
+int main()
+{
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		int input = 0;
+		scanf("%d", &input);
+
+		while (true)
+		{
+			if (_stack.empty() == true)
+			{
+				answer[i] = 0;
+				break;
+			}
+
+			pair<int, int> peekPair = _stack.top();
+			int peek = peekPair.second;
+			if (peek < input)
+			{
+				_stack.pop();
+			}
+			else
+			{
+				answer[i] = peekPair.first;
+				break;
+			}
+		}
+
+		_stack.push(make_pair(i, input));
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d ", answer[i]);
+	}
+
+	return 0;
+}
